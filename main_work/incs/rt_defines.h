9052d3f5b9a56ee2fab25cb708f6247a37b833bb
Arkhavy | ljohnson <ljohnson@z4r10p5.42lyon.fr>
2022-09-04 16:42:16 +0200

    added object movement into the mix, it works on every axis with every objects except planes ..

--- main_work/incs/rt_defines.h
+++ main_work/incs/rt_defines.h
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/25 13:23:22 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/09/03 12:33:28 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/09/04 16:18:45 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,10 @@
 # define PL 2
 # define CY 3
 # define SQ 4
+
+# define AXIS_X 5
+# define AXIS_Y 6
+# define AXIS_Z 7
 
 # define PARSING_CHARSET "ACLsplcy0123456789-+., \n"
 # define INT_CHARSET "0123456789"
--- main_work/srcs/hooks/rt_manage_hooks.c
+++ main_work/srcs/hooks/rt_manage_hooks.c
@@ -6,7 +6,7 @@
 /*   By: ljohnson <ljohnson@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/03 11:56:26 by ljohnson          #+#    #+#             */
-/*   Updated: 2022/09/03 12:00:02 by ljohnson         ###   ########lyon.fr   */
+/*   Updated: 2022/09/04 16:41:40 by ljohnson         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,67 +21,108 @@ int	exit_x(t_master *master)
 	return (0);
 }
 
-// void	move_object(int key, t_master *master)
+// void	rt_update_camera_pos(int key, t_master *master)
 // {
-// 	float	ratio;
-// 	int		i;
-
-// 	i = 0;
-// 	if (key == K_UP || key == K_RIGHT)
-// 	{
-// 		ratio = 0.2;
-// 		master->obj_data->objects[1]->pos->z += 0.2;
-// 		master->obj_data->objects[2]->pos->z += 0.2;
-// 		master->obj_data->objects[3]->pos->z += 0.2;
-// 	}
-// 	else
-// 		ratio = -0.2;
-// 	while (master->obj_data->objects[i])
-// 	{
-// 		if (key == K_UP || key == K_DOWN)
-// 			master->obj_data->objects[i]->pos->z += ratio;
-// 		else
-// 			master->obj_data->objects[i]->pos->x += ratio;
-// 		i++;
-// 	}
+// 	if (key == K_W)
+// 		master->camera->pos->z += 0.1;
+// 	else if (key == K_S)
+// 		master->camera->pos->z -= 0.1;
+// 	else if (key == K_A)
+// 		master->camera->pos->x -= 0.1;
+// 	else if (key == K_D)
+// 		master->camera->pos->x += 0.1;
 // }
-	// if (key == K_UP || key == K_DOWN || key == K_LEFT || key == K_RIGHT)
-	// 	move_object(key, master);
 
-void	rt_update_camera_pos(int key, t_master *master)
+// void	rt_update_camera_dir(int key, t_master *master)
+// {
+// 	if (key == K_I)
+// 		master->camera->dir->y += 0.2;
+// 	else if (key == K_K)
+// 		master->camera->dir->y += -0.2;
+// 	else if (key == K_J)
+// 		master->camera->dir->x += -0.2;
+// 	else if (key == K_L)
+// 		master->camera->dir->x += 0.2;
+// }
+
+static int	rt_update_selected_object(int key, t_master *master, int object)
 {
-	if (key == K_W)
-		master->camera->pos->z += 0.1;
-	else if (key == K_S)
-		master->camera->pos->z -= 0.1;
-	else if (key == K_A)
-		master->camera->pos->x -= 0.1;
-	else if (key == K_D)
-		master->camera->pos->x += 0.1;
+	if (key == K_LEFT)
+	{
+		if (object == -1 || object == 0)
+			return (master->obj_data->lst_size - 1);
+		else
+			return (object - 1);
+	}
+	else
+	{
+		if (object == -1 || object == (int)(master->obj_data->lst_size - 1))
+			return (0);
+		else
+			return (object + 1);
+	}
+	return (-1);
 }
 
-void	rt_update_camera_dir(int key, t_master *master)
+static int	rt_update_selected_axis(int key)
 {
-	if (key == K_I)
-		master->camera->dir->y += 0.2;
-	else if (key == K_K)
-		master->camera->dir->y += -0.2;
-	else if (key == K_J)
-		master->camera->dir->x += -0.2;
-	else if (key == K_L)
-		master->camera->dir->x += 0.2;
+	if (key == K_X)
+		return (AXIS_X);
+	else if (key == K_Y)
+		return (AXIS_Y);
+	else if (key == K_Z)
+		return (AXIS_Z);
+	return (-1);
+}
+
+static void	rt_update_obj_pos(int key, t_master *master, int object, int axis)
+{
+	if (axis == -1 || object == -1)
+		return ;
+	else
+	{
+		if (key == K_UP)
+		{
+			if (axis == AXIS_X)
+				master->obj_data->objects[object]->pos->x += 0.2;
+			else if (axis == AXIS_Y)
+				master->obj_data->objects[object]->pos->y += 0.2;
+			else if (axis == AXIS_Z)
+				master->obj_data->objects[object]->pos->z += 0.2;
+		}
+		else
+		{
+			if (axis == AXIS_X)
+				master->obj_data->objects[object]->pos->x -= 0.2;
+			else if (axis == AXIS_Y)
+				master->obj_data->objects[object]->pos->y -= 0.2;
+			else if (axis == AXIS_Z)
+				master->obj_data->objects[object]->pos->z -= 0.2;
+		}
+	}
 }
 
 int	key_hook(int key, t_master *master)
 {
+	static int	object = -1;
+	static int	axis = -1;
+
 	if (key == K_ESC)
 		exit_x(master);
 	else
 	{
-		if (key == K_W || key == K_S || key == K_A || key == K_D)
-			rt_update_camera_pos(key, master);
-		if (key == K_I || key == K_K || key == K_J || key == K_L)
-			rt_update_camera_dir(key, master);
+		// if (key == K_W || key == K_S || key == K_A || key == K_D)
+		// 	rt_update_camera_pos(key, master);
+		// else if (key == K_I || key == K_K || key == K_J || key == K_L)
+		// 	rt_update_camera_dir(key, master);
+		if (key == K_LEFT || key == K_RIGHT)
+			object = rt_update_selected_object(key, master, object);
+		else if (key == K_X || key == K_Y || key == K_Z)
+			axis = rt_update_selected_axis(key);
+		else if (key == K_UP || key == K_DOWN)
+			rt_update_obj_pos(key, master, object, axis);
+		// else if (key == PV_PLUS || key == PV_MINUS)
+		// 	rt_update_object_size(key, master, object);
 		rt_display_scene(master);
 	}
 	return (0);
